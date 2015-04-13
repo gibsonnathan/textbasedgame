8f06d52b30ad577272e82cb4aa7fcf6e619ff194
Nathan Gibson <gibson.nathan.2011@gmail.com>
2015-04-12 22:02:55 -0400

    minor changes

---
+++ StarterGame/Protocols.h
@@ -0,0 +1,13 @@
+//
+//  Protocols.h
+//  StarterGame
+//
+//  Created by Nathan Gibson on 4/12/15.
+//  Copyright (c) 2015 Ringtuple, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Protocols : NSObject
+
+@end
